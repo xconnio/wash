39bdf517d484d96fbc5849991d86a78b67b06564
asimfarooq5 <asimfarooq5@gmail.com>
2025-08-23 17:59:35 +0500

    Initialize a basic daemon

--- cmd/wshd/main.go
+++ cmd/wshd/main.go
@@ -1,5 +1,44 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/xconnio/xconn-go"
+)
+
 func main() {
-	println("The long-running background service aka wshd")
+	const (
+		defaultRealm = "wampshell"
+		defaultPort  = 8022
+	)
+
+	router := xconn.NewRouter()
+	router.AddRealm(defaultRealm)
+	defer router.Close()
+
+	server := xconn.NewServer(router, nil, nil)
+
+	address := fmt.Sprintf("0.0.0.0:%d", defaultPort)
+	closer, err := server.ListenAndServeRawSocket(xconn.NetworkTCP, address)
+	if err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
+	defer func(closer io.Closer) {
+		err := closer.Close()
+		if err != nil {
+
+		}
+	}(closer)
+
+	log.Printf("wshd running. Realm=%s, Listening on %s", defaultRealm, address)
+
+	closeChan := make(chan os.Signal, 1)
+	signal.Notify(closeChan, os.Interrupt)
+	<-closeChan
+
+	log.Println("Shutting down wshd...")
 }
