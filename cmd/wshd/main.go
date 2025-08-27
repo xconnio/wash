4e4662adfafb500e66e818e28aeec2940a6f9837
asimfarooq5 <asimfarooq5@gmail.com>
2025-08-28 01:22:51 +0500

    Register io.xconn.wampshell.exec procedure for command execution

--- cmd/wshd/main.go
+++ cmd/wshd/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bytes"
+	"context"
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
 	"os/signal"
 
 	"github.com/xconnio/wampshell"
@@ -11,10 +14,47 @@ import (
 )
 
 const (
-	defaultRealm = "wampshell"
-	defaultPort  = 8022
-	defaultHost  = "0.0.0.0"
+	defaultRealm  = "wampshell"
+	defaultPort   = 8022
+	defaultHost   = "0.0.0.0"
+	procedureExec = "wampshell.shell.exec"
 )
+
+func runCommand(cmd string, args ...string) (string, error) {
+	var stdout, stderr bytes.Buffer
+	command := exec.Command(cmd, args...)
+	command.Stdout = &stdout
+	command.Stderr = &stderr
+	err := command.Run()
+	if err != nil {
+		return stderr.String(), err
+	}
+	return stdout.String(), nil
+}
+
+func handleRunCommand(_ context.Context, inv *xconn.Invocation) *xconn.InvocationResult {
+	log.Printf("Received invocation: args=%v, kwargs=%v", inv.Args(), inv.Kwargs())
+
+	cmd, err := inv.ArgString(0)
+	if err != nil {
+		return xconn.NewInvocationError("wamp.error.invalid_argument", err.Error())
+	}
+	rawArgs := inv.Args()[1:]
+	args := make([]string, 0, len(rawArgs))
+	for idx := range rawArgs {
+		str, err := inv.ArgString(idx + 1)
+		if err != nil {
+			return xconn.NewInvocationError("wamp.error.invalid_argument", err.Error())
+		}
+		args = append(args, str)
+	}
+	output, err := runCommand(cmd, args...)
+	if err != nil {
+		return xconn.NewInvocationError("wamp.error.internal_error", err.Error())
+	}
+
+	return xconn.NewInvocationResult(output)
+}
 
 func main() {
 	address := fmt.Sprintf("%s:%d", defaultHost, defaultPort)
@@ -55,7 +95,19 @@ func main() {
 	}
 	defer func() { _ = closer.Close() }()
 
-	fmt.Printf("listening on rs://%s\n", address)
+	log.Printf("listening on rs://%s", address)
+
+	session, err := xconn.ConnectInMemory(router, defaultRealm)
+	if err != nil {
+		log.Fatalf("failed to connect to server: %v", err)
+	}
+
+	registerResponse := session.Register(procedureExec, handleRunCommand).Do()
+	if registerResponse.Err != nil {
+		log.Fatalf("failed to register procedure %q: %v", procedureExec, registerResponse.Err)
+	}
+
+	log.Printf("Procedure registered: %s", procedureExec)
 
 	closeChan := make(chan os.Signal, 1)
 	signal.Notify(closeChan, os.Interrupt)
